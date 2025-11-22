63aa8b3e928d4c92e0c1a29261095666c9f38cf6
thswjdguq <myhyups02@gmail.com>
2025-11-22 19:49:32 +0900

    initcommit - bamtol Market

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:bamtol_market_app/firebase_options.dart';
+import 'package:bamtol_market_app/src/app.dart';
+import 'package:bamtol_market_app/src/common/controller/authentication_controller.dart';
+import 'package:bamtol_market_app/src/common/controller/data_load_controller.dart';
+import 'package:bamtol_market_app/src/home/page/home_page.dart';
+import 'package:bamtol_market_app/src/splash/controller/splash_controller.dart';
+import 'package:bamtol_market_app/src/user/login/page/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+late SharedPreferences prefs;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  prefs = await SharedPreferences.getInstance();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: '당근마켓 클론코딩',
+      initialRoute: '/',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          color: Color(0xff212123),
+          titleTextStyle: TextStyle(color: Colors.white),
+        ),
+        scaffoldBackgroundColor: const Color(0xff212123),
+      ),
+      initialBinding: BindingsBuilder(() {
+        Get.put(SplashController());
+        Get.put(DataLoadController());
+        Get.put(AuthenticationController()); //------1
+      }),
+      getPages: [
+        GetPage(name: '/', page: () => const App()),
+        GetPage(name: '/home', page: () => const HomePage()),
+        GetPage(name: '/login', page: () => const LoginPage()),
+      ],
+    );
+  }
+}
---
+++ lib/src/splash/enum/step_type.dart
@@ -0,0 +1,8 @@
+enum StepType {
+  init(''),
+  dataLoad('데이터 로드'),
+  authCheck('인증 체크');
+
+  const StepType(this.name);
+  final String name;
+}
